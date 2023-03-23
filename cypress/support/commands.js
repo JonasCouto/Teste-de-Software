9154926eef3389fdbbd1bb8e61d805d715da76b5
Jonas Couto <jonas-couto@hotmail.com>
2023-03-23 19:12:41 -0300

    Testes nos dados do formulario

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,45 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('addRecord', (user) => {
+    
+    cy.fixture(user)
+    .then((user) => {
+
+        cy.get('#name')
+        .should('be.visible')
+        .type(user.name)
+        .should('have.value', user.name)
+
+        cy.get('#email')
+        .should('be.visible')
+        .type(user.email)
+        .should('have.value', user.email)
+
+        cy.get('#company')
+        .should('be.visible')
+        .type(user.company)
+        .should('have.value', user.company)
+
+        cy.get('#website')
+        .should('be.visible')
+        .type(user.website)
+        .should('have.value', user.website)
+
+        cy.get('#phone')
+        .should('be.visible')
+        .type(user.phone)
+        .should('have.value', user.phone)
+
+        cy.get('#inquiry')
+        .should('be.visible')
+        .type("Como faço para realizar uma ação?")
+        .should('have.value', user.inquiry)
+
+
+        
+    })
+    
+})
+
