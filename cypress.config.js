bac050dcd644deed8b8fd4ef7f1d0af1c9b5d8f5
Jonas Couto <jonas-couto@hotmail.com>
2023-03-23 19:09:51 -0300

    Nova URL para visit

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "https://www.saucedemo.com/v1/inventory.html",
+    baseUrl: "https://www.pagbrasil/formulario/duvida",
     
     
   },
